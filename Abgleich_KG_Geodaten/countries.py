a234259d3833c1f52a28a44ea2f8f4fa190ff0a5
Maelkolb <111154096+Maelkolb@users.noreply.github.com>
2023-09-27 13:15:24 +0200

    Update countries.py

--- Abgleich_KG_Geodaten/countries.py
+++ Abgleich_KG_Geodaten/countries.py
@@ -1,8 +1,9 @@
 import csv
-from geopy.geocoders import Nominatim
+import geopandas as gpd
+import re
 
 def find_matching_words(input_csv, text_file, output_csv):
-   # Werte der Spalte 'country' zwischenspeichern
+    # Werte der Spalte 'country' zwischenspeichern
     with open(input_csv, 'r') as input_file:
         reader = csv.DictReader(input_file)
         countries = set(row['Country'] for row in reader)
@@ -11,7 +12,9 @@ def find_matching_words(input_csv, text_file, output_csv):
     with open(text_file, 'r') as file:
         text = file.read()
 
-    geolocator = Nominatim(user_agent="myGeocoder")
+    world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
+
+    world['Centroid'] = world.centroid
 
     with open(output_csv, 'w', newline='') as output_file:
         fieldnames = ['Modern Toponym', 'Ancient Toponym', 'Country', 'Longitude (X)', 'Latitude (Y)']
@@ -19,18 +22,19 @@ def find_matching_words(input_csv, text_file, output_csv):
         writer.writeheader()
 
         for country in countries:
-            # Übereinstimmung = Koordinaten abfragen
-            if country in text:
-                location = geolocator.geocode(country)
-                if location:
+            # Verbesserte Ergebnisse
+            pattern = r'\b' + re.escape(country) + r'\b'
+            if re.search(pattern, text, re.IGNORECASE):
+                country_data = world[world['name'] == country]
+                if not country_data.empty:
+                    centroid = country_data['Centroid'].iloc[0]
                     output_row = {
                         'Modern Toponym': '',
                         'Ancient Toponym': '',
                         'Country': country,
-                        'Longitude (X)': location.longitude,
-                        'Latitude (Y)': location.latitude
+                        'Longitude (X)': centroid.x,
+                        'Latitude (Y)': centroid.y
                     }
                     writer.writerow(output_row)
 
-
-find_matching_words('Hanson2016_Cities.csv', 'Text_euseb_KG.txt', 'output_countries.csv')
+find_matching_words('Hanson2016_Cities.csv', 'Text_euseb_KG.txt', 'output_countries_geopandas.csv')
